3e722f314e612e4c3ddc6739bf1c224e6caf75e8
Lucas Martinez <lucasmaximartinez9@gmail.com>
2024-03-19 13:08:53 -0300

    Creacion entidad paciente

---
+++ src/main/java/com/hungytech/capacitacion/models/Paciente.java
@@ -0,0 +1,27 @@
+package com.hungytech.capacitacion.models;
+
+import java.util.Date;
+import java.util.*;
+
+public class Paciente extends Persona{
+    private HistoriaClinica historiaClinica;
+    private String grupoSanguineo;
+
+    public Paciente(int dni, String nombreApellido, Date fechaNacimiento, String direccion, String genero, HistoriaClinica historiaClinica, String grupoSanguineo)
+    {
+        super(dni, nombreApellido, direccion,genero,fechaNacimiento);
+        this.historiaClinica = historiaClinica;
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public String getGrupoSanguineo(){
+        return grupoSanguineo;
+    }
+
+    public List<Estudios> ObtenerHistoriaClinica(dni){
+
+        return 	historiaClinica.getHistoria(dni);
+    }
+
+
+}
